b1fe0a6cebadef25424eb89845aef15f6ea416a0
Henrr Egedegbe <egedegbehenry@gmail.com>
2024-05-17 23:31:55 +0000

    add script.js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
